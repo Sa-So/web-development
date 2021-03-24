17efe704efce2a1c9864f82074d14b0a6021947e
Sa-So <60461699+Sa-So@users.noreply.github.com>
2021-03-25 02:53:57 +0530

    Create fibonacci.js

---
+++ Intermediate JS/fibonacci.js
@@ -0,0 +1,26 @@
+// everytime she says flowchart in 2x feels like she is saying bl*wj*b
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    var out=[];
+    var sum1=0;
+  var f=0;
+  var s=0;
+    //Write your code here:
+    for(var i=0;i<n;i++){
+      sum1=f+s;
+      if(i==0)
+        s++;
+      f=s
+      s=sum1
+      out.push(sum1)
+    }
+    return out;
+    
+    
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
